bb281094382ccdbb8cd5f8b5af07a00c7119d751
Charles Yao <yaoc2@cs.washington.edu>
2019-10-12 14:58:02 -0700

    add router and skeleton profile page code

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 import './App.css';
+import ProfileContainer from './components/profile/ProfileContainer'
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path="/profile">
+                    <ProfileContainer />
+                </Route>
+                <Route path={"/"}>
+                    <p>This is home page</p>
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
---
+++ app/src/components/profile/Profile.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+    render () {
+        return (
+          <div className={"profile-content-container"}>
+              <p>This is profile</p>
+          </div>
+        );
+    }
+}
+
+export default Profile;
