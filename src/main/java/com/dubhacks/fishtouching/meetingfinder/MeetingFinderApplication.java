788e3558a14db6cd8f9e10910ecfc6a25cc2f6cb
Charles Yao <yaoc2@cs.washington.edu>
2019-10-12 12:28:53 -0700

    init

---
+++ src/main/java/com/dubhacks/fishtouching/meetingfinder/MeetingFinderApplication.java
@@ -0,0 +1,13 @@
+package com.dubhacks.fishtouching.meetingfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingFinderApplication.class, args);
+	}
+
+}
