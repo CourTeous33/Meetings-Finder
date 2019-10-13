c49a661a2b15febeea992d576c9bdc6f0296889d
gideonjin <manchj@cs.washington.edu>
2019-10-12 19:17:28 -0700

    updated

--- app/src/ui/InvitePage.js
+++ app/src/ui/InvitePage.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import {Button} from 'antd';
+import {Button, Icon} from 'antd';
+import {Link} from 'react-router-dom';
+import "../styles/InvitePage.css"
 
 export default class InvitePage extends React.Component {
     constructor(props) {
@@ -10,10 +12,34 @@ export default class InvitePage extends React.Component {
     }
 
     render() {
+        const plusStyle = {
+            fontSize: '6em',
+            cursor: 'pointer'
+        }
+
+        const buttonStyle={
+            fontSize: '1.5em',
+            borderRadius: '8pt',
+            cursor: 'pointer',
+            width: '10em'
+        }
         return (
             <div className="InvitePage">
-                <div></div>
-                <Button>Find</Button>
+                <div className="otherThings">
+                    <p>Invite your meeting members!</p>
+                </div>
+                <div className="invitees">
+                    <div className='input'>
+                        <textarea id="textareabox" name="textarea1" placeholder="Username">
+                        </textarea>
+                        <div>
+                            <Icon type="plus-circle" style={plusStyle}/>
+                        </div>
+                    </div>
+                </div>
+                <div className="find">
+                    <Button style={buttonStyle}>Find</Button>
+                </div>
             </div>
         )
     }
