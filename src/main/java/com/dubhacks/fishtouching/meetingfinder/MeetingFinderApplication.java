becbbedec1b134ae74d72ae6bc165f17a0817c25
Shi Nie <nies@uw.edu>
2019-10-12 18:08:16 -0700

    :

--- src/main/java/com/dubhacks/fishtouching/meetingfinder/MeetingFinderApplication.java
+++ src/main/java/com/dubhacks/fishtouching/meetingfinder/MeetingFinderApplication.java
@@ -1,11 +1,15 @@
 package com.dubhacks.fishtouching.meetingfinder;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @SpringBootApplication
 public class MeetingFinderApplication {
-
+    
 	public static void main(String[] args) {
 		SpringApplication.run(MeetingFinderApplication.class, args);
 	}
