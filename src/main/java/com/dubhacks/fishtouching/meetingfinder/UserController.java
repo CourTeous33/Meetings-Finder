f1311357ad751ca2601132e60c9e139b1a31c0b0
Wei Fan <weif3@uw.edu>
2019-10-12 18:14:55 -0700

    remove comment

--- src/main/java/com/dubhacks/fishtouching/meetingfinder/UserController.java
+++ src/main/java/com/dubhacks/fishtouching/meetingfinder/UserController.java
@@ -22,19 +22,19 @@ import java.util.Optional;
 @RequestMapping("/api")
 public class UserController {
 	
-//	@Autowired
-//	private UserRepository userRepository;
-//	
-//	@PostMapping(path="/add") // Map ONLY POST Requests
-//	public @ResponseBody String addNewUser (@RequestParam String name
-//	, @RequestParam String email) {
-//		// @ResponseBody means the returned String is the response, not a view name
-//		// @RequestParam means it is a parameter from the GET or POST request
-//
-//		User n = new User ("123", "ggg", "1234", "gg@gmail.com");
-//		userRepository.save(n);
-//		return "Saved";
-//	}
+	@Autowired
+	private UserRepository userRepository;
+	
+	@PostMapping(path="/add") // Map ONLY POST Requests
+	public @ResponseBody String addNewUser (@RequestParam String name
+	, @RequestParam String email) {
+		// @ResponseBody means the returned String is the response, not a view name
+		// @RequestParam means it is a parameter from the GET or POST request
+
+		User n = new User ("123", "ggg", "1234", "gg@gmail.com");
+		userRepository.save(n);
+		return "Saved";
+	}
 //
 //	@GetMapping(path="/all")
 //	public @ResponseBody Iterable<User> getAllUsers() {
